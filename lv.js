bf44b8192268f95e84358cda2bb025b90229d65a
volvicgeyser <xxx@xxxxx.com>
2018-03-10 14:37:01 +0900

    敵レベル追加プラグイン

---
+++ lv.js
@@ -0,0 +1,19 @@
+﻿/*:
+ * @plugindesc 敵LV設定プラグイン
+ * @author volvicgeyser
+ *
+ * @param default_lv
+ * @type number
+ * @desc 敵のレベルが設定されていない時はこの値となります。
+ * @default 1
+ * @help
+ * 敵にレベルを設定します。
+ * 敵キャラのメモ欄に<lv:値>を記述します。
+ * 例 レベル52に設定する <lv:52>
+ * 計算式でb.lvが使用可能となります。
+ */
+(function() {
+    var parameters = PluginManager.parameters('lv');
+    var default_lv = Number(parameters['default_lv'] || 0);
+    Game_Enemy.prototype.__defineGetter__("lv", function() { return $dataEnemies[this._enemyId].meta.lv || default_lv; });
+})();
